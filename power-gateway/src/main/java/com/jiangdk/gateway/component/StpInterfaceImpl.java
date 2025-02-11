077bc8548246fcd4e00c8392af38dc9528b33a0d
jiangdk <jiang1728439852@gmail.com>
2025-02-12 00:19:18 +0800

    fix:解决网关报错，更改nacos注册配置

--- power-gateway/src/main/java/com/jiangdk/gateway/component/StpInterfaceImpl.java
+++ power-gateway/src/main/java/com/jiangdk/gateway/component/StpInterfaceImpl.java
@@ -1,38 +1,38 @@
-package com.jiangdk.gateway.component;
-
-import cn.dev33.satoken.stp.StpInterface;
-import org.springframework.stereotype.Component;
-
-import java.util.Collections;
-import java.util.List;
-
-/**
- * @author: JiangDk
- * @date: 2024/12/11 20:39
- * @description:
- */
-@Component
-public class StpInterfaceImpl implements StpInterface {
-    /**
-     * 获取权限列表
-     * @param o
-     * @param s
-     * @return
-     */
-    @Override
-    public List<String> getPermissionList(Object o, String s) {
-        return Collections.emptyList();
-    }
-
-    /**
-     * 获取角色列表
-     * @param o
-     * @param s
-     * @return
-     */
-
-    @Override
-    public List<String> getRoleList(Object o, String s) {
-        return Collections.emptyList();
-    }
-}
+//package com.jiangdk.gateway.component;
+//
+//import cn.dev33.satoken.stp.StpInterface;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Collections;
+//import java.util.List;
+//
+///**
+// * @author: JiangDk
+// * @date: 2024/12/11 20:39
+// * @description:
+// */
+//@Component
+//public class StpInterfaceImpl implements StpInterface {
+//    /**
+//     * 获取权限列表
+//     * @param o
+//     * @param s
+//     * @return
+//     */
+//    @Override
+//    public List<String> getPermissionList(Object o, String s) {
+//        return Collections.emptyList();
+//    }
+//
+//    /**
+//     * 获取角色列表
+//     * @param o
+//     * @param s
+//     * @return
+//     */
+//
+//    @Override
+//    public List<String> getRoleList(Object o, String s) {
+//        return Collections.emptyList();
+//    }
+//}
--- power-gateway/src/main/java/com/jiangdk/gateway/config/CorsConfig.java
+++ power-gateway/src/main/java/com/jiangdk/gateway/config/CorsConfig.java
@@ -1,24 +1,6 @@
 package com.jiangdk.gateway.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.cors.reactive.CorsUtils;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
 
-//package com.jiangdk.gateway.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.reactive.CorsWebFilter;
-//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-//
 //@Configuration
 //public class CorsConfig {
 //
@@ -40,27 +22,27 @@ import org.springframework.web.server.WebFilterChain;
 //        return new CorsWebFilter(source);
 //    }
 //}
-@Configuration
-public class CorsConfig {
-    @Bean
-    public WebFilter corsFilter() {
-        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-            ServerHttpRequest request = ctx.getRequest();
-            if (CorsUtils.isCorsRequest(request)) {
-                HttpHeaders requestHeaders = request.getHeaders();
-                ServerHttpResponse response = ctx.getResponse();
-                HttpMethod requestMethod = requestHeaders.getAccessControlRequestMethod();
-                HttpHeaders headers = response.getHeaders();
-                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "*");
-                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
-                headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
-                headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
-                if (requestMethod != null) {
-                    headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, requestMethod.name());
-                }
-            }
-            return chain.filter(ctx);
-        };
-    }
-}
+//@Configuration
+//public class CorsConfig {
+//    @Bean
+//    public WebFilter corsFilter() {
+//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+//            ServerHttpRequest request = ctx.getRequest();
+//            if (CorsUtils.isCorsRequest(request)) {
+//                HttpHeaders requestHeaders = request.getHeaders();
+//                ServerHttpResponse response = ctx.getResponse();
+//                HttpMethod requestMethod = requestHeaders.getAccessControlRequestMethod();
+//                HttpHeaders headers = response.getHeaders();
+//                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+//                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "*");
+//                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
+//                headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
+//                headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
+//                if (requestMethod != null) {
+//                    headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, requestMethod.name());
+//                }
+//            }
+//            return chain.filter(ctx);
+//        };
+//    }
+//}
